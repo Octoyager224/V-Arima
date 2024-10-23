fb6ecc8b1c189a2e3c3aea6c20e88cb9797a1cb3
Arima <eternelarima@gmail.com>
2024-10-23 00:47:31 +0000

    Create imgur.js

---
+++ Framework/imgur.js
@@ -0,0 +1,32 @@
+const axios = require('axios');
+const fs = require('fs');
+const FormData = require('form-data');
+
+async function uploadImageToImgur(imagePath, clientId) {
+  try {
+    const data = new FormData();
+    data.append('image', fs.createReadStream(imagePath));
+
+    const headers = {
+      'Authorization': `Client-ID ${clientId}`,
+      ...data.getHeaders()
+    };
+
+    const config = {
+      method: 'post',
+      maxBodyLength: Infinity,
+      url: 'https://imgur.com/D4C8P63',
+      aheaders: headers,
+      data: data
+    };
+
+    const response = await axios(config);
+    const imageUrl = response.data.data.link;
+    return imageUrl;
+  } catch (error) {
+    console.error('Erreur lors de l\'envoi sur Imgur:', error);
+    throw new Error('Une erreur est survenue lors de l\'envoi sur Imgur.');
+  }
+}
+
+module.exports = { uploadImageToImgur };
